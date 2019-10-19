ea0411c2c604479206539dc2c97595e21d79982c
salihcemil <32238094+salihcemil@users.noreply.github.com>
2019-10-19 17:44:33 +0300

    Add files via upload

---
+++ Business/Crypto.cs
@@ -0,0 +1,50 @@
+﻿using Babel.Licensing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Business.Models;
+
+namespace Business
+{
+    public static class Crypto
+    {
+        public static string GetHash(string ObjectString)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(ObjectString));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+        public static string getSignature(string textToSign, string publicKey, string privateKey)
+        {
+            ECDsaSignature signer;
+            signer = ECDsaSignature.FromKeys(publicKey, privateKey);
+            byte[] byteStr = Encoding.Default.GetBytes(textToSign);
+            byte[] byteSignature = signer.SignData(byteStr);
+
+            return Encoding.Default.GetString(byteSignature);
+        }
+
+        public static bool CheckSignature(Input inp)
+        {
+            List<string> stringList = inp.ScriptSign.Split(' ').ToList();
+            string signature = stringList[0];
+            string receiverPublicKey = stringList[1];
+            string stringToVerify = inp.PrevTransactionHash + " " + inp.OutputIndex.ToString();
+
+            ECDsaSignature verifier = ECDsaSignature.FromKeys(receiverPublicKey);
+            return verifier.VerifyData(Encoding.Default.GetBytes(stringToVerify), Encoding.Default.GetBytes(signature));
+        }
+    }
+}
---
+++ Business/Miner.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Models;
+
+namespace Business
+{
+    public static class Miner
+    {
+        public static Block CreateBlock(List<Transaction> trxList, Header prevBlockHeader)
+        {
+            Block blk = new Block();
+
+            blk.BlockHeader = GenerateHeader(trxList, prevBlockHeader);
+            blk.BlockID = prevBlockHeader.ID + 1;
+            blk.BlockHeader.ID = blk.BlockID;
+            blk.Transactions = trxList;
+            blk.TransactionsCount = trxList.Count;
+
+            return blk;
+        }
+
+        private static Header GenerateHeader(List<Transaction> Transactions, Header prevBlockHeader)
+        {
+            Header header = new Header();
+
+            header.DifficultyIndex = 5;
+            header.HashMerkleRoot = GenerateMerkleRoot(Transactions);
+            header.PrevBlockHash = prevBlockHeader.Hash;
+            header.Time = DateTime.UtcNow;
+
+            long nonce = 0;
+            string starter = "1";
+            string targetValue = "";
+            for (int i = 0; i < header.DifficultyIndex; i++)
+            {
+                targetValue = targetValue + "0";
+            }
+            string hash;
+
+            while (starter != targetValue)
+            {
+                hash = Crypto.GetHash(header.HashMerkleRoot + header.PrevBlockHash + header.Time.ToString() + nonce.ToString());
+                starter = hash.Substring(0, header.DifficultyIndex);
+                if (starter == targetValue)
+                {
+                    header.Hash = hash;
+                    break;
+                }
+                nonce = nonce + 1;
+            }
+
+            header.Nonce = nonce;
+
+            return header;
+        }
+
+        public static string GenerateMerkleRoot(List<Transaction> transactions)
+        {
+            List<string> strList = new List<string>();
+            foreach (var item in transactions)
+            {
+                strList.Add(item.Hash);
+            }
+            if ((strList.Count % 2) == 0)
+            {
+                while (strList.Count > 1)
+                {
+                    strList.Add(Crypto.GetHash(strList[0] + strList[1]));
+                    strList.RemoveAt(0);
+                    strList.RemoveAt(0);
+                }
+            }
+            else
+            {
+                while (strList.Count > 1)
+                {
+                    strList.Insert(strList.Count - 1, Crypto.GetHash(strList[0] + strList[1]));
+                    if (strList.Count != 3) strList.RemoveRange(0, 2);
+                    else
+                    {
+                        strList.RemoveAt(0);
+                        strList.RemoveAt(1);
+                    }
+                }
+            }
+            return strList[0];
+        }
+    }
+}
