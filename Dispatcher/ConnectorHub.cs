5d368cc0c5272c50fd2b31562738db83b946ed4f
salihcemil <32238094+salihcemil@users.noreply.github.com>
2019-10-19 19:11:42 +0300

    Add files via upload

---
+++ Dispatcher/ConnectorHub.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispatcher
+{
+    public class ConnectorHub : Hub
+    {
+        public ConnectorHub()
+        {
+        }
+        public void DetermineLength(string message)
+        {
+            Console.WriteLine("Broadcasting message: " + message);
+            string newMessage = string.Format(@"{0} has a length of: {1}", message, message.Length);
+            Clients.Others.newMessageReceived(message);
+            OnConnected();
+        }
+
+        public void NewMessage(string Message)
+        {
+            Clients.All.newMessageReceived(Message);
+        }
+
+        public override Task OnConnected()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+        public static class UserHandler
+        {
+            public static HashSet<string> ConnectedIds = new HashSet<string>();
+        }
+    }
+}
---
+++ Dispatcher/Startup.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispatcher
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
